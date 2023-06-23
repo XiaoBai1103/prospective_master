ba29e6cf093d6d28e5ea2fa8ef5ba82a8964c323
“XiaoBai1103” <btx@baitian.xin>
2023-06-24 01:36:06 +0800

    数据结构-二叉树的遍历

--- DataStructure/Tree/binaryTree.py
+++ DataStructure/Tree/binaryTree.py
@@ -9,17 +9,17 @@ class Node:
         self.lchild = None
         self.rchild = None
 class Tree:
+    #root必须传入节点，而不是数值
     def __init__(self,root=None):
         self.root = root
-#添加节点(优先添加到左结点)
+    #添加节点
     def add(self, elem):
         node = Node(elem)
         #如果树是空的，则对根节点赋值
         if self.root == None:
             self.root = node
-        else:
-            queue = []
-            queue.append(self.root)
+            return
+        queue = [self.root]
         #对已有的节点进行层次遍历
         while queue:
                 #弹出队列的第一个元素
@@ -27,18 +27,71 @@ class Tree:
                 if cur.lchild == None:
                     cur.lchild = node
                     return
-                elif cur.rchild == None:
+                else:
+                    queue.append(cur.lchild)
+                if cur.rchild == None:
                     cur.rchild = node
                     return
                 else:
-                #如果左右子树都不为空，加入队列继续判断
-                    queue.append(cur.lchild)
+                    queue.append(cur.rchild)
+                    
 
-
-
+    #广度优先遍历
+    def breadth(self):
+        if self.root == None:
+            return
+        queue = [self.root]
+        while queue:
+            cur = queue.pop(0)
+            print(cur.elem,end=' ')
+            if cur.lchild != None:
+                queue.append(cur.lchild)
+            if cur.rchild != None:
+                queue.append(cur.rchild)
+    #深度优先遍历-先序遍历（根左右）
+    def preorder(self,node):
+        if node == None:
+            return
+        print(node.elem,end=' ')
+        self.preorder(node.lchild)
+        self.preorder(node.rchild)
+    #深度优先遍历-中序遍历（左根右)
+    def inorder(self, node):
+        if node == None:
+            return
+        self.inorder(node.lchild)
+        print(node.elem,end=' ')
+        self.inorder(node.rchild)
+    #深度优先遍历-后 序遍历（左右根）
+    def postorder(self, node):
+        if node == None:
+            return
+        self.postorder(node.lchild)
+        self.postorder(node.rchild)
+        print(node.elem,end=' ')
 if __name__ == '__main__':  
-    tree = Tree(4)
+    tree = Tree()
+    tree.add(1)
+    tree.add(2)
     tree.add(3)
+    tree.add(4)
+    tree.add(5)
+    tree.add(6)
+ 
 
-    
-        
+    '''
+                1
+            2       3
+         4     5 6      
+    '''
+    print("广度优先遍历:")
+    tree.breadth()
+    print()
+    print("先序遍历:")
+    tree.preorder(tree.root)
+    print()
+    print("中序遍历:")
+    tree.inorder(tree.root)
+    print()
+    print("后序遍历:")
+    tree.postorder(tree.root)
